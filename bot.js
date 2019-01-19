5927f8398c7be900025ee07993e012e7ef8af0cc
HarleyTBS <37520082+HarleyTBS@users.noreply.github.com>
2019-01-19 16:45:29 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '$channel') {
+    	message.reply('Harley's Channel Is: https://youtube.com/harleytbs');
   	}
 });
 
